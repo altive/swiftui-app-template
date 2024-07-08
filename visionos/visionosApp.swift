18c86d454ea32fff5afbe0687af8cae1128dfd10
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-07-09 05:03:08 +0900

    Initial Commit

---
+++ visionos/visionosApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionosApp.swift
+//  visionos
+//
+//  Created by Riscait on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct visionosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
